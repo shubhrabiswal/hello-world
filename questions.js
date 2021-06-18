b9eac077756ea243edc0f8a3db7729adcf5e7e98
shubhrabiswal <31442710+shubhrabiswal@users.noreply.github.com>
2021-06-18 15:03:18 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,20 @@
-#### factorial
-#### Pallindrome
-#### 
+#### Quadratic equations
+
+function quad(a,b,c){
+  let disc = b*b - 4*a*c;
+  let res;
+  let root1,root2;
+  if(disc == 0){
+    root1 = -b/(2*a);
+    root2 = -b/(2*a);
+  }
+  if(disc>0){
+    root1 = (-b + Math.sqrt(disc))/2*a;
+    root2 = (-b - Math.sqrt(disc))/2*a;
+  }
+  console.log(root1,root2)
+}
+
+quad(1,6,5);
+
+

0a4101367ae96d0f41e2529099f78f04b3eb6858
shubhrabiswal <31442710+shubhrabiswal@users.noreply.github.com>
2021-07-26 22:58:49 +0530

    Update confirm-the-ending.js

--- basic_algorithm_scripting/confirm-the-ending.js
+++ basic_algorithm_scripting/confirm-the-ending.js
@@ -1,3 +1,5 @@
+// check whether the input string ends with same letters as the target
+
 function confirmEnding(str, target) {
   let ind = str.length-target.length
   if(str.substr(ind,target.length)==target){

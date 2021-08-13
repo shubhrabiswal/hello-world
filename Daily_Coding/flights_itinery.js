33c8116e76aa1ad033caf7ce223208af17042b65
shubhrabiswal <31442710+shubhrabiswal@users.noreply.github.com>
2021-08-13 18:34:40 +0530

    Update flights_itinery.js

--- Daily_Coding/flights_itinery.js
+++ Daily_Coding/flights_itinery.js
@@ -1,37 +1,59 @@
+// Given an unordered list of flights taken by someone, each represented as
+//(origin,destination) pairs, and a starting airport, compute the person's itinery
+// If no such itinery exists return null.
+//If there are multiple possible itineries, return the lexicographically samllest one.
+//All flights must be used in the itinery
+
 function get_itinery(flight_list,start){
-    let itinery = []
-    let source = []
-    for(let flight of flight_list){
-        if(flight[0] == start && source.includes(flight[0])==false){
-            // if(itinery.includes(flight[0]))
-            itinery.push(flight[0])
-            itinery.push(flight[1])
-        }
-        source.push(flight[0])
-    }
-
-    let temp = itinery[1];
-    let count = 0;
-    console.log(itinery)
+    let start_source = [];
+    let result = [];
     
-    for(let flight of flight_list){
-        if(flight[0] == temp){
-            // itinery.push(flight[0])
-            console.log("in",flight)
-            itinery.push(flight[1])
-            temp = flight[1]
-            count++
+    for(let i=0;i<flight_list.length;i++){
+        if(flight_list[i][0] == start){
+            start_source.push(flight_list[i])
         }
-        console.log(flight)
     }
-
-    if(count == 0){
-        return null
+    
+    if(start_source.length>1){
+        start_source.sort((a, b) => a[1].localeCompare(b[1]))
+    }
+    // console.log(start_source)
+    result.push(start_source[0][0])
+    result.push(start_source[0][1])
+    let temp = start_source[0][1];
+    let side = []
+    side.push(start_source[0])
+    // console.log(side,"side")
+    let total = 0 
+    for(let i=0;i<flight_list.length;i++){
+        for(let j=0;j<flight_list.length;j++){
+            let count = 0
+            if(flight_list[j][0] == temp){
+                for(let flight of side){
+                    if(flight[0] == flight_list[j][0] && flight[1] == flight_list[j][1] ){
+                       count++
+                    //    console.log(flight_list[j],"if.....")
+                    }
+                }
+                if(count == 0){
+                    side.push(flight_list[j])
+                    result.push(flight_list[j][1])
+                    temp  = flight_list[j][1]
+                    total++
+                    // console.log(flight_list[j],"2nd if.....")
+                }
+            }
+           
+        }
+    }
+    
+    if(total > 0){//} && side.length == flight_list.length){
+        return result
     }else{
-        return itinery
+        return null
     }
+    
 }
-
 console.log(get_itinery([['A','B'],['A','C'],['B','C'],['C','A']],'A'))
-//A B C A C
-//NOT A C A B C
+console.log(get_itinery([['SFO','HKO'],['YYZ','SFO'],['YUL','YYZ'],['HKO','ORD']],'YUL'))
+console.log(get_itinery([['SFO','COM'],['COM','YYZ']],'COM'))

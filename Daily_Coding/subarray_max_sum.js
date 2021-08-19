295b7f5acb9456893805be7c443cef7e7f27bb56
shubhrabiswal <31442710+shubhrabiswal@users.noreply.github.com>
2021-08-19 21:43:38 +0530

    Create subarray_max_sum.js

---
+++ Daily_Coding/subarray_max_sum.js
@@ -0,0 +1,19 @@
+// Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+
+// For example, given the array [34,-50,42,14,-5,86], the maximum sum would be 137, 
+// since we would take elements 42,14,-5,86.
+
+// Given the array [-5,-1,-8,-9], the maximum sum would be 0,
+// since we would not take any elements.
+
+
+function subarray_sum_max(arr){
+    for(let i=0;i<arr.length;i++){
+        let temp = arr[i]
+        for(let j=i+1;j<arr.length;j++){
+            
+        }
+    }
+}
+
+console.log(subarray_sum_max([34,-50,42,14,-5,86]))
